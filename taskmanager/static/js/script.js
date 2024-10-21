0f19ee945502748e9ac67d0197b6c368ade17698
Hedgemonkey <hedgemonkey@gmail.com>
2024-10-21 13:58:26 +0100

    Add functionality to delete categories

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('.modal').modal();
+  // Initialize Modals (using event delegation for dynamic content)
+  $(document).on('click', 'a[href^="#modal-"]', function (e) {
+    e.preventDefault(); // Prevent default link behavior
+    var modalId = $(this).attr('href'); // Get the modal ID from the link's href
+    console.log(modalId);
+    $(modalId).modal('open'); // Open the modal
   });
+});
